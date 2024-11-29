aec9fcedb7e336a56eb53521680f1d22c2f7e2b3
Bruce0203 <BruceFreedy@gmail.com>
2024-11-29 20:51:25 +0900

    support value in enum variant

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -2,16 +2,25 @@
 pub fn var_name(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = syn::parse_macro_input!(input as syn::ItemEnum);
     let enum_name = &input.ident;
-    let variant_names: Vec<_> = input
+    let match_branches = input
         .variants
         .iter()
-        .map(|variant| &variant.ident)
-        .collect();
+        .map(|variant| {
+            let variant_name = &variant.ident;
+            let field_names = (0..variant.fields.len())
+                .map(|_| quote::quote! {_})
+                .collect::<Vec<_>>();
+            println!("{:?}", field_names);
+            quote::quote! {
+                #enum_name::#variant_name(#(#field_names),*) => stringify!(#variant_name)
+            }
+        })
+        .collect::<Vec<_>>();
     quote::quote! {
     impl ename::VariantName for #enum_name {
             fn variant_name(&self) -> &'static str {
                 match self {
-                    #(#enum_name::#variant_names => stringify!(#variant_names),)*
+                    #(#match_branches),*
                 }
             }
         }
--- tests/sandbox.rs
+++ tests/sandbox.rs
@@ -1,13 +1,11 @@
-use ename::VariantName;
-
 #[test]
 fn test() {
-    #[derive(VariantName)]
-    enum TestStruct {
-        A,
-        B,
-        C,
+    #[derive(name_variant::NamedVariant)]
+    enum TestStruct<T, const N: usize> {
+        A(i32),
+        B(T),
+        C(i32, bool),
     }
-    let value = TestStruct::A;
-    value.variant_name();
+    let value = TestStruct::<i32, 123>::A(1);
+    TestStruct::<i32, 123>::variant_name(&value);
 }
